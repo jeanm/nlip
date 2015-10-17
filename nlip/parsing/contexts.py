fe427b289e2b55ab8cbd086e116c5943832e5d7a
Jean Maillard <jean@maillard.it>
2015-10-17 01:16:39 +0100

    simplify get_phrase_window, merge it with get_sentence_window

--- nlip/parsing/contexts.py
+++ nlip/parsing/contexts.py
@@ -1,4 +1,10 @@
 from itertools import zip_longest
+
+def _sort_tuples(indices, window, k):
+    return sorted((tuple(element) for element in enumerate(window)
+            if element[1] <= k and element[0] not in indices),
+            key=lambda x: x[1])
+
 def get_window(index, length, k=2):
     """Return a list of tuples (pos, dist_from_centre) for index's window"""
     start_win = max(0, index - k)
@@ -6,18 +12,8 @@ def get_window(index, length, k=2):
     return sorted(((pos,abs(index-pos)) for pos in range(start_win, end_win+1)
             if pos != index), key=lambda x: x[1])
 
-def get_phrase_window(indices, length, k=2):
-    window = [k+1] * length  # maps position in sentence to smallest distance from index word
-    indices = set(indices)  # used to make sure I don't include indices in the window
-    for index in indices:
-        for pos, distance in get_window(index, length, k):
-            if distance < window[pos]:  # only update distance if lower than what we already found
-                window[pos] = distance
-    # positions are sorted by smallest distance from any index word, and
-    # do not include index words themselves
-    return sorted((tuple(element) for element in enumerate(window)
-            if element[1] <= k and element[0] not in indices),
-            key=lambda x: x[1])
-
-def get_sentence_window(indices, length):
-    return [i for i in range(length) if i not in indices]
+def get_phrase_window(indices, length, k=None):
+    if k is None:
+        k = length
+    distances = (min([abs(l-i) for i in indices]) for l in range(length))
+    return _sort_tuples(indices, distances, k)
